30357ecb4fca5da81cee6377459c864e683db18d
kanika.np@acem.edu.in <kanikakumari.vacso@fynd-external.com>
2022-07-02 18:41:06 +0530

    resolve post error

--- client/src/components/ConnectionFeed.js
+++ client/src/components/ConnectionFeed.js
@@ -7,33 +7,34 @@ import { format } from "timeago.js";
 function ConnectionFeed({ post }) {
   const [like, setLike] = useState(post.likes.length);
   const [isliked, setisliked] = useState(false);
-  const [userName, setUserName] = useState({});
-  const [userProfileImage, setuserProfileImage] = useState({});
+  const [userName, setUserName] = useState();
+  const [userProfileImage, setuserProfileImage] = useState();
 
   useEffect(() => {
     const FetchUser = async () => {
       const users = await axios
         .get(`http://localhost:3100/api/user/${post.userId}`)
         .then((response) => {
-          setUserName(response.data.username || "Anonymous User");
+          setUserName(response.data.username);
           setuserProfileImage(response.data.profilePicture || false);
         });
     };
     FetchUser();
-  }, [userName]);
+  }, []);
   const likeHandler = () => {
     setLike(isliked ? like - 1 : like + 1);
     setisliked(!isliked);
   };
   return (
-    <div className="connectionFeedContainer">
+    <div className="connectionFeedContainer" key={post._id}>
       <div className="connectionFeedTop">
         <img
           className="connectionProfileImage"
           src={userProfileImage || "/assets/person/noavatar.png"}
           alt=""
         />
-        <span className="connectionName">Kanika</span>
+
+        <span className="connectionName">{userName || "No User"}</span>
         <span className="postTime">{format(post.createdAt, "en_US")}</span>
         <span className="moreView">
           <MoreVert> </MoreVert>
